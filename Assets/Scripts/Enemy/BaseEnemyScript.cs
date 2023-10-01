ed28dd806b081116948b1cd1ca14a746e5805448
gakikami <145588900+gakikami@users.noreply.github.com>
2023-10-01 21:11:46 +0800

    enemy

---
+++ Assets/Scripts/Enemy/BaseEnemyScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemyScript : MonoBehaviour
+{
+    public float enemyHp = 1f;
+    public float enemyMoveSpeedX = 1f;
+    public float enemyMoveSpeedY = 1f;
+    public 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
