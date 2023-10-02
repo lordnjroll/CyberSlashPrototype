4a9b889ae11104cff3f8dfc04520b95fde200c85
gakikami <145588900+gakikami@users.noreply.github.com>
2023-10-03 01:02:59 +0800

    AI

--- Assets/Scripts/Enemy/BaseEnemyScript.cs
+++ Assets/Scripts/Enemy/BaseEnemyScript.cs
@@ -1,23 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BaseEnemyScript : MonoBehaviour
 {
     public float enemyHp = 1f;
     public float enemyMoveSpeedX = 1f;
     public float enemyMoveSpeedY = 1f;
-    public 
+    public Transform target;
+    public float test = 1f;
+
+    public float attankRange;
+    private float pathUpdateDeadline;
+    private EnemyManager enemyManager;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        attankRange = enemyManager.navMeshAgent.stoppingDistance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target != null)
+        {
+            //Run to target(Player)
+            bool inRange = Vector3.Distance(transform.position, target.position) <= attankRange;
+
+            if (inRange)
+            {
+                LookAtTarget();
+            }
+            else
+            {
+                UpdatePath();
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        enemyManager = GetComponent<EnemyManager>();
+    }
+
+    public void LookAtTarget()
+    {
+        //Find Player
+        Vector3 lookPos = target.position - transform.position;
+        lookPos.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.2f);
+    }
+
+    public void UpdatePath()
+    {
+        navMeshagent.SetDestination(target.position);\
+
+        if (Time.time >= pathUpdateDeadline)
+        {
+            pathUpdateDeadline = Time.time + enemyManager.pathUpdateDelay;
+            enemyManager.navMeshAgent.SetDestination(target.position);
+        }
+    }
+
+    
+
+    void attank()
+    {
+
+    }
+
+    void Move()
+    {
+
+    }
+
+    void Death()
+    {
+        if (enemyHp == 0)
+        {
+
+        }
     }
 }
---
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyManager : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public Animator animator;
+
+    public float pathUpdateDelay = 0.2f;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+}
