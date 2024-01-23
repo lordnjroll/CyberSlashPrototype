7b660ba905e6da49ed54acd60657e1d3c2330147
lordnjroll <jefftoui@gmail.com>
2024-01-24 00:08:28 +0800

    Launch Script

--- Assets/Scripts/Neutral/LaunchWave.cs
+++ Assets/Scripts/Neutral/LaunchWave.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShockWave : MonoBehaviour
+public class LaunchWave : MonoBehaviour
 {
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +18,6 @@ public class ShockWave : MonoBehaviour
     {
         
     }
+
+
 }
